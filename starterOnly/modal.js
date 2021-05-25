2344a679c4fccfd7e5676f3579846d0fec7a17df
Mark Stevens <markspstevens@yahoo.co.uk>
2021-05-25 12:30:35 +0200

    name check error corrected

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -75,7 +75,7 @@ function closeMessage() {
 
 // CHECK FIRST & LAST NAMES ARE VALID FUNCTION
 function checkString(string, name){
-  if (!nameRegex.test(string.trim()) || string.length < 2){
+  if (!nameRegex.test(string.trim()) || string.trim().length < 2){
     name.textContent ="Veuillez entrer un minimum de 2 caractères (pas de caractères spéciaux).";   /*Veuillez entrer 2 caractères ou plus pour ce champ.*/
   } else {
     name.textContent ="";} 
@@ -153,12 +153,12 @@ document.getElementById("checkbox1").addEventListener("change", ($event) => {
 // FORM VALIDATION FUNCTION
 function validate(event) {
     event.preventDefault();
-    if (!firstName.value || !nameRegex.test(firstName.value.trim()) || firstName.value.trim() < 2) {
+    if (!firstName.value || !nameRegex.test(firstName.value.trim()) || firstName.value.trim().length < 2) {
       firstNameError.textContent ="Veuillez entrer votre prénom (pas de caractères spéciaux)";
       firstName.focus();
       return false;
     }
-    if (!lastName.value || !nameRegex.test(lastName.value.trim()) || lastName.value.trim() < 2) {
+    if (!lastName.value || !nameRegex.test(lastName.value.trim())  || lastName.value.trim().length < 2) {
       lastNameError.textContent ="Veuillez entrer votre nom (pas de caractères spéciaux)";
       lastName.focus();
       return false;
